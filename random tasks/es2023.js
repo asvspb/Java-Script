b554b0d98fbf780373aeee86c907d991b39fb8c9
asv-spb <asv.devpro@gmail.com>
2023-06-30 19:35:00 +0300

    findlast

--- random tasks/es2023.js
+++ random tasks/es2023.js
@@ -1,7 +1,7 @@
 const _array = [1, 2, 3, 4, 9];
 
-const findElement = _array.findLast(i => {
-    console.log(i);
-    return i == 9
+const findElement = _array.findLast(element => {
+    console.log(element);
+    return element == 9
 })
 console.log(findElement);
