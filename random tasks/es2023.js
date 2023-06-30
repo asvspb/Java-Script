660a9404eaa59f6bbf6ae998f5ef7e690a38aeea
asv-spb <asv.devpro@gmail.com>
2023-06-30 19:50:08 +0300

    new array with some changes

--- random tasks/es2023.js
+++ random tasks/es2023.js
@@ -1,7 +1,15 @@
 const _array = [1, 2, 3, 4, 9];
 
+// method returns the last element in the array that satisfies the given predicate
 const findElement = _array.findLast(element => {
     console.log(element);
     return element == 9
 })
-console.log(findElement);
+
+// новый массив из старого
+const __array = [1, 2, 3, 4, 9];
+// const new__array = [...__array];
+// new__array[new__array.length - 1] = 5;
+// console.log(new__array);
+const new__array = __array.with(-1, 5); //?
+
